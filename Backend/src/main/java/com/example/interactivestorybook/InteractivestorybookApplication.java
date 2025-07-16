3b455c05ea68b4618496a84ade8ca25b786f6e35
Anushap976 <mail2anusha14@gmail.com>
2025-07-16 00:07:42 -0500

    Initial SpringBoot project with dependencies

---
+++ Backend/src/main/java/com/example/interactivestorybook/InteractivestorybookApplication.java
@@ -0,0 +1,13 @@
+package com.example.interactivestorybook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InteractivestorybookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InteractivestorybookApplication.class, args);
+	}
+
+}
---
+++ Backend/src/test/java/com/example/interactivestorybook/InteractivestorybookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.interactivestorybook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InteractivestorybookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
