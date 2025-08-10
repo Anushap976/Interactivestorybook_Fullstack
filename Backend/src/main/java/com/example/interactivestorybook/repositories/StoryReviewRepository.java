c28f8944e0ba46ab1a635527786630ce08f02df0
Anushap976 <mail2anusha14@gmail.com>
2025-08-10 18:38:59 -0500

    Story review repository for operations

--- Backend/src/main/java/com/example/interactivestorybook/repositories/StoryReviewRepository.java
+++ Backend/src/main/java/com/example/interactivestorybook/repositories/StoryReviewRepository.java
@@ -1,9 +1,11 @@
 package com.example.interactivestorybook.repositories;
-
+import com.example.interactivestorybook.models.Story;
 import com.example.interactivestorybook.models.StoryReview;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
 public interface StoryReviewRepository extends JpaRepository<StoryReview, Long> {
+    List<StoryReview> findByStory(Story story);
+//    List<StoryReview> findByStory_Id(Long storyId);
 }
