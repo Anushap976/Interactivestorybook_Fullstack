d110acf67787665050f779eecb468351bed8aafb
Anushap976 <mail2anusha14@gmail.com>
2025-08-10 12:56:56 -0500

    Created feedback repository for CRUD

---
+++ Backend/src/main/java/com/example/interactivestorybook/repositories/FeedbackRepository.java
@@ -0,0 +1,10 @@
+package com.example.interactivestorybook.repositories;
+
+import com.example.interactivestorybook.models.Feedback;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface FeedbackRepository extends JpaRepository<Feedback, Long> {
+    // No additional methods needed for basic CRUD
+}
