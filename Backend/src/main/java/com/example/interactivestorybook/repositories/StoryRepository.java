cdfd314b7afd0e1558b1404be62167bbfc63ba48
Anushap976 <mail2anusha14@gmail.com>
2025-08-10 14:05:53 -0500

    Story repository interface for database operations

--- Backend/src/main/java/com/example/interactivestorybook/repositories/StoryRepository.java
+++ Backend/src/main/java/com/example/interactivestorybook/repositories/StoryRepository.java
@@ -1,7 +1,8 @@
 package com.example.interactivestorybook.repositories;
-
+import java.util.Optional;
 import com.example.interactivestorybook.models.Story;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface StoryRepository extends JpaRepository<Story, Long> {
+    Optional<Story> findByTitleIgnoreCase(String title);
 }
