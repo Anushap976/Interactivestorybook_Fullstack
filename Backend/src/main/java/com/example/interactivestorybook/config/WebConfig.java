aed4d33b2fa0524e9c7bf05cbd8bf1959014d97f
Anushap976 <mail2anusha14@gmail.com>
2025-08-10 20:50:52 -0500

    added comments for webconfig

--- Backend/src/main/java/com/example/interactivestorybook/config/WebConfig.java
+++ Backend/src/main/java/com/example/interactivestorybook/config/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/**")
-                .allowedOrigins("http://localhost:5175") // your frontend dev server
+                .allowedOrigins("http://localhost:5175") // frontend dev server
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*");
     }
